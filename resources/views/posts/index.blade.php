99a6d5b73de08b5d3a2a228c9fd0863df5339cdb
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-09-13 09:18:41 +0800

    Update

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,11 +5,11 @@
                 <x-authentication-card-logo />
             </div>
 
-            <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg prose dark:prose-invert">
+            <div class="mt-6 p-6 bg-white dark:bg-gray-800 shadow-md ">
                 {{ $posts->links() }}
 
                 @foreach ($posts as $post)
-                    <li>{{ $post->content }}</li>
+                    <li class="my-2">{{ $post->content }}</li>
                 @endforeach
 
                 {{ $posts->links() }}
