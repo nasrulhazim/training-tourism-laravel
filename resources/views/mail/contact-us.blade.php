44d49ab00305da811ff9739f874ee90cac777537
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-09-14 12:05:56 +0800

    Create Mail

---
+++ resources/views/mail/contact-us.blade.php
@@ -0,0 +1,32 @@
+<x-mail::message>
+Hi,
+
+I hope this email finds you well. I am writing to inquire about [briefly explain the reason for contacting]. I have some questions and feedback that I would like to share with your team.
+
+[If it's an inquiry, briefly explain your inquiry or request. If it's feedback, mention the specific product or service you're providing feedback on.]
+
+I appreciate your prompt attention to my inquiries and feedback. Please find my questions and comments listed below:
+
+Inquiry:
+
+[Insert your first question or request here. Be concise and clear.]
+
+[Insert your second question or request here, if applicable.]
+
+Feedback:
+
+[Describe your first feedback point or suggestion.]
+
+[Provide your second feedback point or suggestion, if applicable.]
+
+I would appreciate it if you could provide me with a response or acknowledge receipt of this email within [mention a reasonable timeframe, e.g., 48 hours]. Your assistance in this matter would be greatly appreciated.
+
+If you require any additional information or clarification from my end, please do not hesitate to reach out to me at [your email address] or [your phone number].
+
+Thank you for your time and attention. I look forward to hearing from you soon and resolving my inquiry or contributing to the improvement of your services.
+
+Sincerely,
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
